473c13e109af1437f2f43390ebbb1b1648f52fc9
Vitalik <petergerinv@gmail.com>
2022-06-09 23:05:55 +0300

    feat: Add homework-1

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+    <div v-if="spinner"
+         class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
+      <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+           viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>
+    </div>
     <div class="container">
       <div class="w-full my-4"></div>
       <section>
@@ -12,6 +21,7 @@
               <input
                   v-model="ticker"
                   @keydown.enter="add"
+                  @keyup="tickerSelectChanged"
                   type="text"
                   name="wallet"
                   id="wallet"
@@ -19,6 +29,18 @@
                   placeholder="Например DOGE"
               />
             </div>
+            <div v-if="tickerSuggestions.length" class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
+            <span v-for="suggestion in tickerSuggestions"
+                  :key="suggestion"
+                  @click="tickerSuggestionClicked(suggestion)"
+                  class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              {{ suggestion }}
+            </span>
+
+            </div>
+
+            <div v-if="tickerAlreadySelected" class="text-sm text-red-600">Такой тикер уже добавлен</div>
+
           </div>
         </div>
         <button
@@ -44,7 +66,7 @@
       </section>
 
       <template v-if="tickers.length">
-        <hr class="w-full border-t border-gray-600 my-4" />
+        <hr class="w-full border-t border-gray-600 my-4"/>
         <div>
           <button
               class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
@@ -60,9 +82,9 @@
           >
             Вперед
           </button>
-          <div>Фильтр: <input v-model="filter" /></div>
+          <div>Фильтр: <input v-model="filter"/></div>
         </div>
-        <hr class="w-full border-t border-gray-600 my-4" />
+        <hr class="w-full border-t border-gray-600 my-4"/>
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
               v-for="t in paginatedTickers"
@@ -97,12 +119,14 @@
                     fill-rule="evenodd"
                     d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
                     clip-rule="evenodd"
-                ></path></svg
-              >Удалить
+                ></path>
+              </svg
+              >
+              Удалить
             </button>
           </div>
         </dl>
-        <hr class="w-full border-t border-gray-600 my-4" />
+        <hr class="w-full border-t border-gray-600 my-4"/>
       </template>
       <section v-if="selectedTicker" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
@@ -164,7 +188,7 @@
 // [x] График сломан если везде одинаковые значения
 // [x] При удалении тикера остается выбор
 
-import { subscribeToTicker, unsubscribeFromTicker } from "./api";
+import {subscribeToTicker, unsubscribeFromTicker} from "./api";
 
 export default {
   name: "App",
@@ -179,11 +203,15 @@ export default {
 
       graph: [],
 
-      page: 1
+      page: 1,
+      tickerAlreadySelected: false,
+      spinner: true,
+      coinList: [],
+      tickerSuggestions: [],
     };
   },
 
-  created() {
+  async created() {
     const windowData = Object.fromEntries(
         new URL(window.location).searchParams.entries()
     );
@@ -216,6 +244,13 @@ export default {
     }
 
     setInterval(this.updateTickers, 5000);
+
+    const f = await fetch(
+        'https://min-api.cryptocompare.com/data/all/coinlist?summary=true'
+    );
+    const data = await f.json();
+    this.spinner = false;
+    this.coinList = Object.values(data.Data);
   },
 
   computed: {
@@ -284,7 +319,10 @@ export default {
         name: this.ticker,
         price: "-"
       };
-
+      if (this.tickers.find(t => t.name.toUpperCase() === this.tickerUpperCase())) {
+        this.tickerAlreadySelected = true;
+        return;
+      }
       this.tickers = [...this.tickers, currentTicker];
       this.ticker = "";
       this.filter = "";
@@ -292,7 +330,30 @@ export default {
           this.updateTicker(currentTicker.name, newPrice)
       );
     },
+    clearTickerAlreadySelected() {
+      this.tickerAlreadySelected = false;
+    },
+    tickerSuggestionClicked(suggestion) {
+      this.ticker = suggestion
+      this.add()
+    },
 
+    tickerSelectChanged() {
+      this.clearTickerAlreadySelected();
+      if (this.ticker.length === 0) {
+        this.tickerSuggestions = []
+        return;
+      }
+      const suggestions = this.coinList.filter(coin =>
+          coin.Symbol.toUpperCase().includes(this.tickerUpperCase()) ||
+          coin.FullName.toUpperCase().includes(this.tickerUpperCase()))
+
+      this.tickerSuggestions = suggestions.slice(0, 4).map(t => t.Symbol)
+    },
+
+    tickerUpperCase() {
+      return this.ticker.toUpperCase()
+    },
     select(ticker) {
       console.log(ticker);
       this.selectedTicker = ticker;
@@ -335,6 +396,7 @@ export default {
           `${window.location.pathname}?filter=${value.filter}&page=${value.page}`
       );
     }
-  }
+  },
+
 };
 </script>
