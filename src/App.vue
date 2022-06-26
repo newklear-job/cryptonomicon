e2364729cab9c86c5dd083c5008c6401c0417e4e
Vitalik <petergerinv@gmail.com>
2022-06-26 23:57:12 +0300

    feat: Move graph to a separate component.

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,11 @@
     </div>
     <div class="container">
       <div class="w-full my-4"></div>
-      <add-ticker @add-ticker="add" @ticker-select-changed="clearTickerAlreadySelected" :disabled="tooManyTickersAdded" :coin-list="coinList" :ticker-already-selected="tickerAlreadySelected"/>
+      <add-ticker @add-ticker="add" @ticker-select-changed="clearTickerAlreadySelected" :disabled="tooManyTickersAdded"
+                  :coin-list="coinList" :ticker-already-selected="tickerAlreadySelected"/>
 
       <template v-if="tickers.length">
-        <hr class="w-full border-t border-gray-600 my-4" />
+        <hr class="w-full border-t border-gray-600 my-4"/>
         <div>
           <button
               class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
@@ -30,9 +31,9 @@
           >
             Вперед
           </button>
-          <div>Фильтр: <input v-model="filter" /></div>
+          <div>Фильтр: <input v-model="filter"/></div>
         </div>
-        <hr class="w-full border-t border-gray-600 my-4" />
+        <hr class="w-full border-t border-gray-600 my-4"/>
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
               v-for="t in paginatedTickers"
@@ -68,56 +69,21 @@
                     fill-rule="evenodd"
                     d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
                     clip-rule="evenodd"
-                ></path></svg
-              >Удалить
+                ></path>
+              </svg
+              >
+              Удалить
             </button>
           </div>
         </dl>
-        <hr class="w-full border-t border-gray-600 my-4" />
+        <hr class="w-full border-t border-gray-600 my-4"/>
       </template>
-      <section v-if="selectedTicker" class="relative">
-        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ selectedTicker.name }} - USD
-        </h3>
-        <div
-            class="flex items-end border-gray-600 border-b border-l h-64"
-            ref="graph"
-        >
-          <div
-              v-for="(bar, idx) in normalizedGraph"
-              :key="idx"
-              :style="{ height: `${bar}%` }"
-              class="bg-purple-800 border w-10"
-          ></div>
-        </div>
-        <button
-            @click="selectedTicker = null"
-            type="button"
-            class="absolute top-0 right-0"
-        >
-          <svg
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              xmlns:svgjs="http://svgjs.com/svgjs"
-              version="1.1"
-              width="30"
-              height="30"
-              x="0"
-              y="0"
-              viewBox="0 0 511.76 511.76"
-              style="enable-background:new 0 0 512 512"
-              xml:space="preserve"
-          >
-            <g>
-              <path
-                  d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                  fill="#718096"
-                  data-original="#000000"
-              ></path>
-            </g>
-          </svg>
-        </button>
-      </section>
+      <price-graph v-if="selectedTicker"
+                   :selected-ticker="selectedTicker"
+                   :graph="normalizedGraph"
+                   @closeClicked="selectedTicker = null"
+                   @maxGraphElementsChanged="maxGraphElementsChanged"
+      />
     </div>
   </div>
 </template>
@@ -140,15 +106,17 @@
 // [x] График сломан если везде одинаковые значения
 // [x] При удалении тикера остается выбор
 
-import { subscribeToTicker, unsubscribeFromTicker } from "./api";
+import {subscribeToTicker, unsubscribeFromTicker} from "./api";
 import AddTicker from "./components/AddTicker.vue";
+import PriceGraph from "./components/PriceGraph.vue";
 import constants from './constants';
 
 export default {
   name: "App",
 
   components: {
-    AddTicker
+    AddTicker,
+    PriceGraph
   },
 
   data() {
@@ -208,14 +176,6 @@ export default {
     this.coinList = Object.values(data.Data);
   },
 
-  mounted() {
-    window.addEventListener("resize", this.calculateMaxGraphElements);
-  },
-
-  beforeUnmount() {
-    window.removeEventListener("resize", this.calculateMaxGraphElements);
-  },
-
   computed: {
     tooManyTickersAdded() {
       return this.tickers.length > 4;
@@ -263,23 +223,19 @@ export default {
   },
 
   methods: {
-    hasNoPrice (price) {
+    maxGraphElementsChanged(newMaxGraphElements) {
+      this.maxGraphElements = newMaxGraphElements;
+    },
+    hasNoPrice(price) {
       return price === constants.NO_PRICE;
     },
     clearTickerAlreadySelected(newTicker) {
-      if (newTicker === '')
-      {
+      if (newTicker === '') {
         return;
       }
       this.tickerAlreadySelected = false;
     },
-    calculateMaxGraphElements() {
-      if (!this.$refs.graph) {
-        return;
-      }
 
-      this.maxGraphElements = this.$refs.graph.clientWidth / 38;
-    },
 
     updateTicker(tickerName, price) {
       this.tickers
@@ -333,8 +289,6 @@ export default {
   watch: {
     selectedTicker() {
       this.graph = [];
-
-      this.$nextTick().then(this.calculateMaxGraphElements);
     },
 
     tickers(newValue, oldValue) {
